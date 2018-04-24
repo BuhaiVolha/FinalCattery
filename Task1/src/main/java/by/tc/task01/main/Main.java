5f0d1bc4425a8876a7fd4f9c821b33db9501e929
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-04-24 14:41:58 +0300

    Update Main.java

--- Task1/src/main/java/by/tc/task01/main/Main.java
+++ Task1/src/main/java/by/tc/task01/main/Main.java
@@ -20,10 +20,9 @@ public class Main {
             //////////////////////////////////////////////////////////////////////////////
             // searching for a particular type with certain criteria
 
-            System.out.println("\nИщем определенный тип с критерием");
             Criteria<GoodsType.Oven> criteriaOven = new Criteria<>(GoodsType.Oven.class);
-		    criteriaOven.add(GoodsType.Oven.CAPACITY, 33);
-		    criteriaOven.add(GoodsType.Oven.HEIGHT, 45);
+	    criteriaOven.add(GoodsType.Oven.CAPACITY, 33);
+	    criteriaOven.add(GoodsType.Oven.HEIGHT, 45);
 
             foundGoods = service.find(criteriaOven);
 
@@ -33,19 +32,17 @@ public class Main {
             // searching for any type with certain criteria
             // for example, both Oven and Refrigerator have Width=70
 
-            System.out.println("\nИщем Любой тип с критерием");
             Criteria<GoodsType.Any> criteriaAny = new Criteria<>(GoodsType.Any.class);
             criteriaAny.add(GoodsType.Any.WIDTH, 70);
 
-		    foundGoods = service.find(criteriaAny);
+	    foundGoods = service.find(criteriaAny);
 
-		    PrintGoodsInfo.print(foundGoods);
+	    PrintGoodsInfo.print(foundGoods);
 
             //////////////////////////////////////////////////////////////////////////////
             // searching for any type without any criteria
             // (it will find all goods from the file)
 
-            System.out.println("\nИщем все");
             Criteria<GoodsType.Any> criteriaNone = new Criteria<>(GoodsType.Any.class);
 
             foundGoods = service.find(criteriaNone);
