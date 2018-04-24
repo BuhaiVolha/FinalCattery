2de91036603b277a14c831d9c9ededc1aeff8b8b
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-04-25 02:23:14 +0300

    скобки смещаются

--- Task1/src/main/java/by/tc/task01/service/validation/Validator.java
+++ Task1/src/main/java/by/tc/task01/service/validation/Validator.java
@@ -9,30 +9,30 @@ public class Validator {
     private static final String STRING_VALUES = "OS,WAND_TYPE,FILTER_TYPE,BAG_TYPE," +
             "COLOR,FREQUENCY_RANGE,TITLE,AUTHOR,SUBJECT,PERIODICITY,PAID_OR_FREE";
 	
-	public static <E> void validateCriteria(Criteria<E> criteria) throws ValidationFailedException {
+    public static <E> void validateCriteria(Criteria<E> criteria) throws ValidationFailedException {
 
 	    if (criteria == null) {
 	        throw new NullCriteriaException("The criteria is null!");
-        }
+            }
 	    Map<String, String> parameters = criteria.getCriteria();
 
 		for (Map.Entry<String, String> parameter : parameters.entrySet()) {
 
 		    if (!STRING_VALUES.contains(parameter.getKey())) {
-                try {
-                    double numberValue = Double.parseDouble(parameter.getValue());
+                	try {
+                    		double numberValue = Double.parseDouble(parameter.getValue());
 
-                    if (numberValue < 0) {
-                        throw new NegativeValueException("The value "
-                                + numberValue
-                                + " is negative!");
-                    }
-                } catch (NumberFormatException e) {
-                    throw new NotNumberException("The value "
+                   		if (numberValue < 0) {
+                        	throw new NegativeValueException("The value "
+                                	+ numberValue
+                                	+ " is negative!");
+                    		}
+               	 	} catch (NumberFormatException e) {
+                   	throw new NotNumberException("The value "
                             + parameter.getValue()
                             + " must be a number!");
                 }
             }
         }
-	}
+    }
 }
