03cbfdd5dd6fbb4ffea98baa13a7095b6d9b1903
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-04-24 04:56:04 +0300

    minor fix

--- Task1/src/main/java/by/tc/task01/entity/criteria/Parameters.java
+++ Task1/src/main/java/by/tc/task01/entity/criteria/Parameters.java
@@ -41,46 +41,4 @@ public final class Parameters {
             TITLE, PERIODICITY, PAID_OR_FREE
         }
     }
-
-//    private Parameters() {}
-//
-//
-//    public static enum Oven {
-//        POWER_CONSUMPTION, WEIGHT, CAPACITY, DEPTH, HEIGHT, WIDTH
-//    }
-//
-//    public static enum Laptop {
-//        BATTERY_CAPACITY, OS, MEMORY_ROM, SYSTEM_MEMORY, CPU, DISPLAY_INCHES
-//    }
-//
-//    public static enum Refrigerator {
-//        POWER_CONSUMPTION, WEIGHT, FREEZER_CAPACITY, OVERALL_CAPACITY, HEIGHT, WIDTH
-//    }
-//
-//    public static enum VacuumCleaner {
-//        POWER_CONSUMPTION, FILTER_TYPE, BAG_TYPE, WAND_TYPE, MOTOR_SPEED_REGULATION, CLEANING_WIDTH
-//    }
-//
-//    public static enum TabletPC {
-//        BATTERY_CAPACITY, DISPLAY_INCHES, MEMORY_ROM, FLASH_MEMORY_CAPACITY, COLOR
-//    }
-//
-//    public static enum Speakers {
-//        POWER_CONSUMPTION, NUMBER_OF_SPEAKERS, FREQUENCY_RANGE, CORD_LENGTH
-//    }
-//
-//    public static enum TextBook {
-//        TITLE, SUBJECT, AUTHOR, NUMBER_OF_PAGES
-//    }
-//
-//    public static enum Newspaper {
-//        TITLE, PERIODICITY, PAID_OR_FREE
-//    }
-//
-//    public static enum Any {
-//        TITLE, POWER_CONSUMPTION, WEIGHT,
-//    }
-//
-//    private Parameters() {}
 }
-
