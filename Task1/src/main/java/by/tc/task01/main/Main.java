323fcc53769ebf04cd3fdfe4881014df71cdfb8e
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-04-25 02:17:33 +0300

    just formatting

--- Task1/src/main/java/by/tc/task01/main/Main.java
+++ Task1/src/main/java/by/tc/task01/main/Main.java
@@ -24,10 +24,9 @@ public class Main {
             //////////////////////////////////////////////////////////////////////////////
             // searching for a particular type with certain criteria
 
-            System.out.println("\nИщем определенный тип с критерием");
             Criteria<GoodsType.Oven> criteriaOven = new Criteria<>(GoodsType.Oven.class);
-		    criteriaOven.add(GoodsType.Oven.CAPACITY, 33);
-		    criteriaOven.add(GoodsType.Oven.HEIGHT, 45);
+	    criteriaOven.add(GoodsType.Oven.CAPACITY, 33);
+	    criteriaOven.add(GoodsType.Oven.HEIGHT, 45);
 
             foundGoods = service.find(criteriaOven);
 
@@ -37,19 +36,17 @@ public class Main {
             // searching for any type with certain criteria
             // for example, both Oven and Refrigerator have Width=70
 
-            System.out.println("\nИщем Любой тип с критерием");
             Criteria<GoodsType.Any> criteriaAny = new Criteria<>(GoodsType.Any.class);
             criteriaAny.add(GoodsType.Any.WIDTH, 70);
 
-		    foundGoods = service.find(criteriaAny);
+	    foundGoods = service.find(criteriaAny);
 
-		    PrintGoodsInfo.print(foundGoods);
+	    PrintGoodsInfo.print(foundGoods);
 
             //////////////////////////////////////////////////////////////////////////////
             // searching for any type without any criteria
             // (it will find all goods from the file)
 
-            System.out.println("\nИщем все");
             Criteria<GoodsType.Any> criteriaNone = new Criteria<>(GoodsType.Any.class);
 
             foundGoods = service.find(criteriaNone);
@@ -59,15 +56,12 @@ public class Main {
             //////////////////////////////////////////////////////////////////////////////
             // searching for a certain type with string criteria
 
-            System.out.println("\nИщем опред тип со строковым критерием");
             Criteria<GoodsType.Laptop> criteriaWithString = new Criteria<>(GoodsType.Laptop.class);
             criteriaWithString.add(GoodsType.Laptop.OS, "Windows");
 
             foundGoods = service.find(criteriaWithString);
 
             PrintGoodsInfo.print(foundGoods);
-
-            //////////////////////////////////////////////////////////////////////////////
 
         } catch (TaskException e) {
             System.out.println(e.getMessage());
