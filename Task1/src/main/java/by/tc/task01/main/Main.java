6d63954d4b76a6e7f104a951e45913f6703886b8
BuhaiVolha <kira-shinigami@mail.ru>
2018-04-25 15:01:17 +0300

    logs added

--- Task1/src/main/java/by/tc/task01/main/Main.java
+++ Task1/src/main/java/by/tc/task01/main/Main.java
@@ -1,18 +1,21 @@
 package by.tc.task01.main;
 
+import by.tc.task01.service.GoodsService;
+import by.tc.task01.service.ServiceFactory;
 import by.tc.task01.entity.Goods;
 import by.tc.task01.entity.criteria.Criteria;
 
 import static by.tc.task01.entity.criteria.Parameters.*;
 
 import by.tc.task01.exception.TaskException;
-
-import by.tc.task01.service.GoodsService;
-import by.tc.task01.service.ServiceFactory;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
 
@@ -23,7 +26,8 @@ public class Main {
             GoodsService service = factory.getGoodsService();
 
             //////////////////////////////////////////////////////////////////////////////
-            // searching for a particular type with certain criteria
+
+            logger.log(Level.INFO,"Searching for a particular type with certain criteria");
 
             Criteria<GoodsType.Oven> criteriaOven = new Criteria<>(GoodsType.Oven.class);
             criteriaOven.add(GoodsType.Oven.CAPACITY, 33);
@@ -34,8 +38,9 @@ public class Main {
             PrintGoodsInfo.print(foundGoods);
 
             //////////////////////////////////////////////////////////////////////////////
-            // searching for any type with certain criteria
-            // for example, both Oven and Refrigerator have Width=70
+
+            logger.log(Level.INFO,"Searching for any type with certain criteria\n" +
+                    "for example, both Oven and Refrigerator have Width=70");
 
             Criteria<GoodsType.Any> criteriaAny = new Criteria<>(GoodsType.Any.class);
             criteriaAny.add(GoodsType.Any.WIDTH, 70);
@@ -45,8 +50,9 @@ public class Main {
             PrintGoodsInfo.print(foundGoods);
 
             //////////////////////////////////////////////////////////////////////////////
-            // searching for any type without any criteria
-            // (it will find all goods from the file)
+
+            logger.log(Level.INFO,"Searching for any type without any criteria\n" +
+                    "(it will find all goods from the file)");
 
             Criteria<GoodsType.Any> criteriaNone = new Criteria<>(GoodsType.Any.class);
 
@@ -55,7 +61,8 @@ public class Main {
             PrintGoodsInfo.print(foundGoods);
 
             //////////////////////////////////////////////////////////////////////////////
-            // searching for a certain type with string criteria
+
+            logger.log(Level.INFO,"Searching for a certain type with string criteria");
 
             Criteria<GoodsType.Laptop> criteriaWithString = new Criteria<>(GoodsType.Laptop.class);
             criteriaWithString.add(GoodsType.Laptop.OS, "Windows");
