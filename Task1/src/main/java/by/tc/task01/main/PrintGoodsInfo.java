a4d806e43615928f2d48847697021d381a8526b2
BuhaiVolha <kira-shinigami@mail.ru>
2018-04-26 00:50:03 +0300

    added logger in printInfo

--- Task1/src/main/java/by/tc/task01/main/PrintGoodsInfo.java
+++ Task1/src/main/java/by/tc/task01/main/PrintGoodsInfo.java
@@ -1,20 +1,23 @@
 package by.tc.task01.main;
 
 import by.tc.task01.entity.Goods;
-import by.tc.task01.exception.NoItemFoundException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class PrintGoodsInfo {
+    private static final Logger logger = LogManager.getLogger();
     private static final String NOT_FOUND_MESSAGE = "No items were found";
 
-    public static void print(List<Goods> goods) throws NoItemFoundException {
+    public static void print(List<Goods> goods) {
         if (goods != null && !goods.isEmpty()) {
             for (Goods g : goods) {
                 System.out.println(g);
             }
         } else {
-            throw new NoItemFoundException(NOT_FOUND_MESSAGE);
+            logger.log(Level.INFO, NOT_FOUND_MESSAGE);
         }
     }
 }
