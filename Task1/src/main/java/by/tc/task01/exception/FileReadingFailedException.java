cdd39918d340e9abadaab9d4d4fd7aa9126b4e8a
BuhaiVolha <32500184+BuhaiVolha@users.noreply.github.com>
2018-04-26 15:19:59 +0300

    Update and rename FileNotFoundException.java to FileReadingFailedException.java

---
+++ Task1/src/main/java/by/tc/task01/exception/FileReadingFailedException.java
@@ -0,0 +1,12 @@
+package by.tc.task01.exception;
+
+public class FileReadingFailedException extends TaskException {
+
+    public FileReadingFailedException(String msg) {
+        super(msg);
+    }
+
+    public FileReadingFailedException() {
+
+    }
+}
